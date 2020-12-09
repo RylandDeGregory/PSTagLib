832df007acb7d9ed69e7a03046e08457f5235d9e
RylandDeGregory <rylanddegregory@me.com>
2020-12-08 23:01:11 -0500

    Final style updates

--- Set-Mp3Tags.ps1
+++ Set-Mp3Tags.ps1
@@ -43,45 +43,45 @@ function Get-MP3Directory {
         .SYNOPSIS
             Determine if a provided filesystem directory is valid. Optionally allow users to launch a graphical filesystem browser to select a directory.
     #>
-        [CmdletBinding()]
-        param (
-            [Parameter(Mandatory = $false)]
-            [switch] $Gui # Compatible with Windows PowerShell ONLY. Whether or not to use a Windows forms graphical interface to browse for a directory.
-        )
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [switch] $Gui # Compatible with Windows PowerShell ONLY. Whether or not to use a Windows forms graphical interface to browse for a directory.
+    )
 
-        begin {
-            if ($Gui) {
-                Write-Host -ForegroundColor Blue "What is the path to the folder you want to process?"
-                Start-Sleep -Milliseconds 500
-                [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
-                $OpenFileDialog = New-Object System.Windows.Forms.FolderBrowserDialog
-                $OpenFileDialog.RootFolder = 'MyComputer'
-                $OpenFileDialog.ShowDialog() | Out-Null
+    begin {
+        if ($Gui) {
+            Write-Host -ForegroundColor Blue "What is the path to the folder you want to process?"
+            Start-Sleep -Milliseconds 500
+            [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+            $OpenFileDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+            $OpenFileDialog.RootFolder = 'MyComputer'
+            $OpenFileDialog.ShowDialog() | Out-Null
 
-                # Save selected filesystem path to variable
-                $Directory = $OpenFileDialog.SelectedPath
-            } else {
-                $Directory = Read-Host "What is the path to the folder you want to process?"
-            }
+            # Save selected filesystem path to variable
+            $Directory = $OpenFileDialog.SelectedPath
+        } else {
+            $Directory = Read-Host "What is the path to the folder you want to process?"
         }
+    }
 
-        process {
-            # Ensure that the filesystem path supplied is valid
-            try {
-                $ValidDirectory = Test-Path -Path "$Directory"
-            } catch {
-                throw "Error checking filesystem path supplied $($Error[0])"
-            }
+    process {
+        # Ensure that the filesystem path supplied is valid
+        try {
+            $ValidDirectory = Test-Path -Path "$Directory"
+        } catch {
+            throw "Error checking filesystem path supplied $($Error[0])"
         }
+    }
 
-        end {
-            if ($ValidDirectory) {
-                return $Directory
-            } else {
-                Write-Host -ForegroundColor Red 'Invalid folder path. Please validate and try again.'
-                return $false
-            }
+    end {
+        if ($ValidDirectory) {
+            $Directory
+        } else {
+            Write-Host -ForegroundColor Red 'Invalid folder path. Please validate and try again.'
+            $false
         }
+    }
 } #endfunction Get-MP3Directory
 function Set-MP3MetadataTags {
 <#
@@ -223,7 +223,7 @@ try {
     }
 
     # Inform user of script completion
-    Write-Host -ForegroundColor Green "Complete."
+    Write-Host -ForegroundColor Green 'Complete.'
 } catch {
     throw "Process failed. Please review errors: $Error"
 }
