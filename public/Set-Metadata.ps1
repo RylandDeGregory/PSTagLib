1f470abd40ea1e91b3af38df7a854425678eb696
Ryland DeGregory <rylanddegregory@me.com>
2022-09-06 20:36:28 -0400

    Add Set-Metadata function

---
+++ public/Set-Metadata.ps1
@@ -0,0 +1,115 @@
+function Set-Metadata {
+    <#
+        .SYNOPSIS
+            Set ID3 tag values.
+        .DESCRIPTION
+            Set ID3 tag values using TagLibSharp.
+            Parameters can be supplied by name, by position, or using the pipeline. See examples.
+        .INPUTS
+            Filesystem path, hashtable of ID3 tag names and values.
+        .OUTPUTS
+            PSCustomObject containing filesystem path and updated ID3 tag values.
+        .EXAMPLE
+            Set-Metadata -File '~/Music/Trance/Above & Beyond/Tri-State/Indonesia.mp3' -Property @{'Year' = '2005'; 'Publisher' = 'Anjunabeats'}
+        .EXAMPLE
+            Set-Metadata './Download.flac' -Property @{'Album' = 'Counting The Points'; 'Artist' = 'Andrew Bayer'}
+   #>
+    [CmdletBinding()]
+    [OutputType([System.Object[]])]
+    param (
+        # Filesystem path
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
+        [string] $File,
+
+        # Property or list of properties to return from the file
+        [Parameter(Mandatory, Position = 1)]
+        [hashtable] $Property
+    )
+
+    begin {
+        Write-Verbose 'Entering Set-Metadata Function'
+
+        $ValidProperties = @(
+            'AudioBitrate',
+            'AudioChannels',
+            'AudioSampleRate',
+            'BitsPerSample',
+            'Codecs',
+            'Duration'
+        )
+
+        $ValidTags = @(
+            'Album',
+            'AlbumArtists',
+            'Artists',
+            'BeatsPerMinute',
+            'Comment',
+            'Composers',
+            'Conductor',
+            'Copyright',
+            'DateTagged',
+            'Description',
+            'Disc',
+            'DiscCount',
+            'Genres',
+            'IsCompilation',
+            'Length',
+            'Lyrics',
+            'Performers',
+            'Pictures',
+            'Publisher',
+            'RemixedBy',
+            'Subtitle',
+            'Title',
+            'Track',
+            'Version',
+            'Year'
+        )
+    }
+
+    process {
+        if (Test-Path $File) {
+            try {
+                $FileTags = [TagLib.File]::Create($File)
+            } catch {
+                if ($_.Exception.Message -eq 'Unable to find type [TagLib.File].') {
+                    Write-Error 'TagLibSharp library is not imported. Please ensure this function can access [TagLibSharp.dll]'
+                    return
+                } else {
+                    Write-Error "Failed instantiating TagLib.File class for file [$File]: $_"
+                    return
+                }
+            }
+                Write-Verbose "Setting ID3 metadata tags for file [$File]"
+                $MetadataObject = [PSCustomObject]@{
+                    File = [string]$File
+                }
+                foreach ($MetadataProperty in $Property) {
+                    if ($MetadataProperty -in $ValidProperties) {
+                        Write-Verbose "Setting [$($MetadataProperty.Key)] property to [$($MetadataProperty.Value)] for file [$File]"
+                        $FileTags.Property.$(MetadataProperty.Key) = $MetadataProperty.Value
+                    } elseif ($MetadataProperty -in $ValidTags) {
+                        Write-Verbose "Setting [$($MetadataProperty.Key)] tag to [$($MetadataProperty.Value)] for file [$File]"
+                        $FileTags.Tags.$(MetadataProperty.Key) = $MetadataProperty.Value
+                    } else {
+                        Write-Warning "Property [$($MetadataProperty.Key)] is not a valid property or tag for file [$File]."
+                        Write-Warning "Valid Properties: '$($ValidProperties -join ', ')'. Valid Tags: '$($ValidTags -join ', ')'."
+                    }
+                }
+                try {
+                    $FileTags.Save()
+                } catch {
+                    Write-Error "Failed setting metadata tags for file [$File]: $_"
+                    return
+                }
+                return $MetadataObject
+        } else {
+            Write-Error "File [$File] is not valid filesystem path or does not exist"
+            return
+        }
+    }
+
+    end {
+        Write-Verbose 'Exiting Get-Metadata Function'
+    }
+}
