a50e53cdccc10e9e8cad6668bf5e7df4be81eb52
Ryland DeGregory <rylanddegregory@me.com>
2022-10-17 21:56:52 -0400

    Ensure that relative paths are resolved

--- PSTagLib/public/Set-Title.ps1
+++ PSTagLib/public/Set-Title.ps1
@@ -34,8 +34,9 @@ function Set-Title {
 
     process {
         if (Test-Path $File) {
+            $FilePath = Resolve-Path -Path $File
             try {
-                $FileTags = [TagLib.File]::Create($File)
+                $FileTags = [TagLib.File]::Create($FilePath)
             } catch {
                 if ($_.Exception.Message -eq 'Unable to find type [TagLib.File].') {
                     Write-Error 'TagLibSharp library is not imported. Please ensure this function can access [TagLibSharp.dll]'
